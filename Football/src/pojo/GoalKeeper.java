0eab54961d2c2fa8aaf38b8d0af8920fc9ca0747
Jarret Fernandes <jarret@unigoa.ac.in>
2018-07-16 16:49:41 +0530

    [jarretfernandes]: characteristics for goalkeeper

--- Football/src/pojo/GoalKeeper.java
+++ Football/src/pojo/GoalKeeper.java
@@ -1,11 +1,23 @@
 package pojo;
 
 public class GoalKeeper extends Player {
+	private Boolean hasGlowes;
 	
 	public void catchBall() {
 		System.out.println("catchBall");
 	}
+	public GoalKeeper() {
+		super();
+		hasGlowes = true;
+	}
 	public GoalKeeper(String fullName, Integer jerseyNumber) {
 		super(fullName, jerseyNumber);
+		hasGlowes = true;
+	}
+	public Boolean getHasGlowes() {
+		return hasGlowes;
+	}
+	public void setHasGlowes(Boolean hasGlowes) {
+		this.hasGlowes = hasGlowes;
 	}
 }
