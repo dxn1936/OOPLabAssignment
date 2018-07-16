41f61a74662db8efcd938b8711f6ee38990322e3
Jarret Fernandes <jarret@unigoa.ac.in>
2018-07-16 15:41:01 +0530

    [jarretfernandes]: factorial without recursion

--- LabWork/src/factorial/App.java
+++ LabWork/src/factorial/App.java
@@ -3,7 +3,8 @@ package factorial;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println(Factorial.calculateFactorial(5));
+		System.out.println(Factorial.calculateFactorial_recursion(6));
+		System.out.println(Factorial.calculateFactorial(6));
 	}
 
 }
--- LabWork/src/factorial/Factorial.java
+++ LabWork/src/factorial/Factorial.java
@@ -2,12 +2,19 @@ package factorial;
 
 public class Factorial {
 	
-	public static Integer calculateFactorial(Integer number) {
+	public static Integer calculateFactorial_recursion(Integer number) {
 		if (number == 0) { 
 			return 1;    
 		}
 		else { 
-			return(number * calculateFactorial(number - 1));   
+			return(number * calculateFactorial_recursion(number - 1));   
 		}
+	}
+	public static Integer calculateFactorial(Integer number) {
+		Integer factorial = 1;
+		while(number > 1) {
+			factorial *= (number--);
+		}
+		return factorial;
 	}
 }
